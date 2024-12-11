7e7d5db0ebe6385d966548c76fe328571303ef46
Pavani2469 <143803520+Pavani2469@users.noreply.github.com>
2024-12-11 11:10:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  // Correctly import BrowserRouter, Routes, and Route
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import CollegeHomepage from './Pages/CollegeHomepage/CollegeHomepage';  // Make sure to import the CollegeHomepage component
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="layout-container">
+          <Sidebar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<CollegeHomepage />} /> {/* Replaced Calendar with CollegeHomepage */}
+              {/* Additional routes can be added here */}
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
